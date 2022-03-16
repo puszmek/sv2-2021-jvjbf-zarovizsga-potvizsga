9242b696f69203b3630965a45c820a69dad97467
he <bgyhenrietta@yahoo.com>
2022-03-16 06:39:51 +0100

    numbers

---
+++ src/main/java/numbers/ArmstrongNumbers.java
@@ -0,0 +1,23 @@
+package numbers;
+
+public class ArmstrongNumbers {
+
+    public boolean isArmstrongNumber(int number) {
+        validateNumber(number);
+        int sum = 0;
+        int numberOfDigits = Integer.toString(number).length();
+        int original = number;
+        while (number != 0) {
+            int digit = number % 10;
+            number = number / 10;
+            sum += Math.pow(digit, numberOfDigits);
+        }
+        return sum == original;
+    }
+
+    private void validateNumber(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Number can't be negative: " + number);
+        }
+    }
+}
